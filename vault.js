bd8be589e1d77b99ade843a3d61c7a68f17b2c89
Reyn-L <Reyn.Leong@gmail.com>
2017-06-17 15:43:37 -1000

    Passed first 2 tests

--- vault.js
+++ vault.js
@@ -1,4 +1,29 @@
+/*jshint esversion: 6 */
 'use strict';
-module.exports = function() {
-  
-};
+function vaultGenerator() {
+   var test = {};
+  function setValue(key, value){
+
+      //test[key]= value;
+      //return test;
+  }
+  function getValue(key){
+    if(!key){
+      return null;
+    } else if( key !== test.key){
+      return null;
+    }
+
+  }
+  return{
+    getValue: getValue,
+    setValue : setValue
+  };
+}
+
+module.exports = vaultGenerator;
+var vault = vaultGenerator();
+vault.setValue("cat", "meow");
+
+vault.setValue("dog", "ruff");
+console.log(vault.getValue("cat"));
