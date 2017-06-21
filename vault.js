722c363af244b6bf84a5735127107a4c44d085f2
Reyn-L <Reyn.Leong@gmail.com>
2017-06-20 20:32:17 -1000

    used hasOwnProperty to shorten the process in less steps

--- vault.js
+++ vault.js
@@ -1,39 +1,25 @@
 /*jshint esversion: 6 */
 'use strict';
 function vaultGenerator() {
-   let obj = {};
-  function setValue(key, value){
+ let obj = {};
+ function setValue(key, value){
 
-      obj[key]= value;
-      return obj;
-  }
-  function getValue(key){
-    //if no key is used, return null
-    if(!key){
-      return null;
-    //setting my value from setValue function to null
-    } else if (obj[key] === undefined) {
-      return null;
-    } else {
-      //if those don't apply return value
-      return obj[key];
-    }
-/*  if(obj[key]){
-      return obj[key];
-    } else {
-      return null;
-    }*/
-
- }
-  return{
-    getValue : getValue,
-    setValue : setValue
-  };
+  obj[key]= value;
 }
 
-/*Did not understand why this worked, can I get an explanation?
-Willing to redo all of this to get a better understanding
-or find out better ways to solve this*/
+function getValue(key){
+  if(obj.hasOwnProperty(key)){
+    return obj[key];
+  } else{
+    return null;
+  }
+}
+
+return{
+  getValue : getValue,
+  setValue : setValue
+};
+}
 
 
 
